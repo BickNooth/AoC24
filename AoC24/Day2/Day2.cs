40f822128baa08e47a3dda60cfefc2288caf0a8c
Nicholas Booth <nicholas.booth@maples.com>
2024-12-02 15:29:53 +0000

    Day2 Part2

--- AoC24/Day2/Day2.cs
+++ AoC24/Day2/Day2.cs
@@ -19,7 +19,7 @@ public class Part1
         Console.WriteLine("Safe Reports: "+safeReports);
     }
 
-    private bool CheckDifferences(Report report)
+    public static bool CheckDifferences(Report report)
     {
         var levels = report.Levels;
         return levels.Zip(levels.Skip(1), (a, b) => 
@@ -27,7 +27,7 @@ public class Part1
                      .All(x => x);
     }
 
-    private bool CheckIncreaseOrDecrease(Report report)
+    public static bool CheckIncreaseOrDecrease(Report report)
     {
         var levels = report.Levels;
         var isIncreasing = levels.Zip(levels.Skip(1), (a, b) => a <= b).All(x => x);
@@ -50,47 +50,34 @@ public class Part2
         var safeReports = 0;
         foreach (var report in Reports)
         {
-            var (passedDifferences, removedValueDifferences) = CheckDifferences(report);
-            var (passedUpOrDown, removedUpOrDown) = CheckIncreaseOrDecrease(report);
-            if (passedDifferences && passedUpOrDown)
+            var passedDifference = Part1.CheckDifferences(report);
+            var passedIncrease = Part1.CheckIncreaseOrDecrease(report);
+
+            if (passedDifference && passedIncrease)
             {
-                var removals = Convert.ToInt32(removedValueDifferences) + Convert.ToInt32(removedUpOrDown);
-                if (removals < 2)
+                safeReports++;
+            }
+            else
+            {
+                for (var i = 0; i < report.Levels.Count(); i++)
                 {
-                    safeReports++;
+                    var levelsCopy = report.Levels.ToList();
+                    levelsCopy.RemoveAt(i);
+                    var newReport = new Report(levelsCopy);
+
+                    var passedDifferenceDampened = Part1.CheckDifferences(newReport);
+                    var passedIncreaseDampened = Part1.CheckIncreaseOrDecrease(newReport);
+                    
+                    if (passedDifferenceDampened && passedIncreaseDampened)
+                    {
+                        safeReports++;
+                        break;
+                    }
                 }
             }
         }
 
         Console.WriteLine("Safe Reports: " + safeReports);
-    }
-
-    private (bool, bool) CheckDifferences(Report report)
-    {
-        var levels = report.Levels;
-        var differences= levels.Zip(levels.Skip(1), (a, b) =>
-                        Math.Abs(a - b) >= 1 && Math.Abs(a - b) <= 3).ToArray();
-        var passes = differences.Count(x => x);
-        var limit = differences.Length - 1;
-        return (passes >= limit, passes != limit);
-    }
-
-    private (bool, bool) CheckIncreaseOrDecrease(Report report)
-    {
-        var levels = report.Levels;
-
-        var increasings = levels.Zip(levels.Skip(1), (a, b) => a <= b).ToArray();
-        var passesIncrease = increasings.Count(x => x);
-        var limitIncrease = increasings.Length - 1;
-        if(passesIncrease >= limitIncrease)
-        {
-            return (passesIncrease >= limitIncrease, passesIncrease != limitIncrease);
-        }
-
-        var decreasings = levels.Zip(levels.Skip(1), (a, b) => a >= b).ToArray();
-        var passesDecrease = decreasings.Count(x => x);
-        var limitDecrease = decreasings.Length - 1;
-        return (passesDecrease >= limitDecrease, passesDecrease != limitDecrease);
     }
 }
 

51334b913c53c1f024741799cf557b8662c983bd
Nicholas Booth <nicholas.booth@maples.com>
2024-12-02 16:09:35 +0000

    Add reflective startup

--- AoC24/Day2/Day2.cs
+++ AoC24/Day2/Day2.cs
@@ -2,21 +2,55 @@
 
 public record Report(List<int> Levels);
 
-public class Part1
+public class Day2
 {
-    private List<Report> Reports { get; }
+    private const string FileName = "input.txt";
+    private List<Report> Reports { get; set; } = null!;
 
-    public Part1(string fileName)
+    public void Part1()
     {
-        Reports = Inputs.ParseFile(fileName);
-    }
-
-    public void Run()
-    {
+        Reports = Inputs.ParseFile(FileName);
         var safeReports = Reports.Count(
             report => CheckIncreaseOrDecrease(report) && CheckDifferences(report));
 
         Console.WriteLine("Safe Reports: "+safeReports);
+    }
+
+    public void Part2()
+    {
+        Reports = Inputs.ParseFile(FileName);
+
+        var safeReports = 0;
+        foreach (var report in Reports)
+        {
+            var passedDifference = CheckDifferences(report);
+            var passedIncrease = CheckIncreaseOrDecrease(report);
+
+            if (passedDifference && passedIncrease)
+            {
+                safeReports++;
+            }
+            else
+            {
+                for (var i = 0; i < report.Levels.Count(); i++)
+                {
+                    var levelsCopy = report.Levels.ToList();
+                    levelsCopy.RemoveAt(i);
+                    var newReport = new Report(levelsCopy);
+
+                    var passedDifferenceDampened = CheckDifferences(newReport);
+                    var passedIncreaseDampened = CheckIncreaseOrDecrease(newReport);
+
+                    if (passedDifferenceDampened && passedIncreaseDampened)
+                    {
+                        safeReports++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Safe Reports: " + safeReports);
     }
 
     public static bool CheckDifferences(Report report)
@@ -33,51 +67,6 @@ public class Part1
         var isIncreasing = levels.Zip(levels.Skip(1), (a, b) => a <= b).All(x => x);
         var isDecreasing = levels.Zip(levels.Skip(1), (a, b) => a >= b).All(x => x);
         return isIncreasing || isDecreasing;
-    }
-}
-
-public class Part2
-{
-    private List<Report> Reports { get; }
-
-    public Part2(string fileName)
-    {
-        Reports = Inputs.ParseFile(fileName);
-    }
-
-    public void Run()
-    {
-        var safeReports = 0;
-        foreach (var report in Reports)
-        {
-            var passedDifference = Part1.CheckDifferences(report);
-            var passedIncrease = Part1.CheckIncreaseOrDecrease(report);
-
-            if (passedDifference && passedIncrease)
-            {
-                safeReports++;
-            }
-            else
-            {
-                for (var i = 0; i < report.Levels.Count(); i++)
-                {
-                    var levelsCopy = report.Levels.ToList();
-                    levelsCopy.RemoveAt(i);
-                    var newReport = new Report(levelsCopy);
-
-                    var passedDifferenceDampened = Part1.CheckDifferences(newReport);
-                    var passedIncreaseDampened = Part1.CheckIncreaseOrDecrease(newReport);
-                    
-                    if (passedDifferenceDampened && passedIncreaseDampened)
-                    {
-                        safeReports++;
-                        break;
-                    }
-                }
-            }
-        }
-
-        Console.WriteLine("Safe Reports: " + safeReports);
     }
 }
 
--- AoC24/Program.cs
+++ AoC24/Program.cs
@@ -1,4 +1,53 @@
-﻿var part = new AoC24.Day2.Part2("Day2\\input.txt");
-part.Run();
+﻿using System.Reflection;
 
-Console.ReadKey();
+namespace AoC24;
+
+public class Program
+{
+    static void Main()
+    {
+        // Get the current date
+        var today = DateTime.Today;
+        var day = today.Day;
+        var year = today.Year - 2000;
+
+        // Construct the namespace and class names
+        var namespaceName = $"AoC{year}.Day{day}";
+        var dayClassName = $"{namespaceName}.Day{day}";
+
+        // Load the assembly (assuming the current assembly)
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Get the type of the Day class
+        var dayType = assembly.GetType(dayClassName);
+        if (dayType == null)
+        {
+            Console.WriteLine($"Class {dayClassName} not found.");
+            return;
+        }
+
+        // Create an instance of the Day class
+        var instance = Activator.CreateInstance(dayType);
+
+        // Check if Part2 method exists
+        var part2Method = dayType.GetMethod("Part2");
+        if (part2Method != null)
+        {
+            // Run Part2
+            part2Method.Invoke(instance, null);
+        }
+        else
+        {
+            // Run Part1
+            var part1Method = dayType.GetMethod("Part1");
+            if (part1Method == null)
+            {
+                Console.WriteLine($"Part1 method not found in class {dayClassName}.");
+                return;
+            }
+            part1Method.Invoke(instance, null);
+        }
+
+        Console.ReadKey();
+    }
+}
